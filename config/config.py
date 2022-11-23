5dbb57501b5eaf81fad7b8ccd3653bfc1edab8b5
olegtititele <96022759+olegtititele@users.noreply.github.com>
2022-11-24 00:16:42 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,17 +1,25 @@
 TOKEN = "5784942663:AAECc4A38Zj0k5bGBL0put2T8aDGtYfiHUI" # Токен бо▒^bа>
 
-bot_description = "Welcome"
+bot_description = "<b>🤖 Создай своего бота автопродаж фоток и видео за 10 минут! \n\nТы сможешь:\n-Загружать и удалять материал\n-Ставить свой ценник\n-Выводить деньги на QIWI, ЮMoney\n\n@FansBuy - канал\n@FansBuyHelpBot - тех. поддержка</b>"
 
-QIWI_PRIV_KEY = "eyJ2ZXJzaW9uIjoiUDJQIiwiZGF0YSI6eyJwYXlpbl9tZXJjaGFudF9zaXRlX3VpZCI6Ijg1b25icC0wMCIsInVzZXJfaWQiOiI3OTI2NTA5MjkyNSIsInNlY3JldCI6ImExM2E1OTJhZTVmNjljMWUyMzUyYmQwZDk4ZTg5ODU0NmFjN2ViMDg5ZDIzNGNjYWVhNDFhYWE3ZGY4YTg5YjcifX0="
+QIWI_PRIV_KEY = "eyJ2ZXJzaW9uIjoiUDJQIiwiZGF0YSI6eyJwYXlpbl9tZXJjaGFudF9zaXRlX3VpZCI6ImVjdzN0ei0wMCIsInVzZXJfaWQiOiI3OTM5NzU0MDI4MiIsInNlY3JldCI6IjIxYmUwNTJiMTEwMTJjYWYxZGVjMTAyMmIyMDZkYTkzODU2ZjE0ZWE5MjEwMGVhNzBiMDJkZTBiMTYyOGIwNjMifX0="
 
-YOOMONEY_PRIV_KEY = "4100117423400817.559D26A9C5B019FA396B775AA49DF73C13E87D0462AE3B9617F589F5A8CB7D92037EAC6BEC72CC3842F9E7B91FD1BE63F9019D22F1F22D33CB8B5E557792E1F74A03A73F5B6B565D2D17B7CBEDDDDFC379FFF2C5A1420E396BBDAB6F7D2408D326087429EC11977435C4642C0681255A90051B96D8CB6E522E88C8632BEE807E"
+YOOMONEY_PRIV_KEY = "4100118026646591.E6E275CCECA78F6EABD31AA7C12ACF2B165AFB62126CF87EB2A2C16ED3218E64"
 
-YOOMONEY_CARD = "4100117423400817"
+YOOMONEY_CARD = "4100118026646591"
 
 admins_chat_id = [2029023685, 5215133538] # id админов
 
-logs_chat_id = -610247150 # канал куда приходят заявки на вывод
+logs_chat_id = -1001619616716 # канал куда приходят заявки на вывод
+
+unverified_material_chat_id = -1001825725091
 
 page = 1
 
 current_material = 0
+
+price_for_one_month = 99 # цена за месяц
+
+price_for_three_month = 279 # цена за 3 месяца
+
+price_for_six_month = 499 # цена за 6 месяцев

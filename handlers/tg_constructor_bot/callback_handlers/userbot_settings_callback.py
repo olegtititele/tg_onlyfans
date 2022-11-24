a3af056ca4ec3be293fa340d792af3b14cc4cf41
olegtititele <96022759+olegtititele@users.noreply.github.com>
2022-11-25 00:19:47 +0300

    Update userbot_settings_callback.py

--- handlers/tg_constructor_bot/callback_handlers/userbot_settings_callback.py
+++ handlers/tg_constructor_bot/callback_handlers/userbot_settings_callback.py
@@ -31,10 +31,9 @@ async def userbot_settings_callback(call, chat_id, message_id):
         photo = images[cf.current_material][0]
         media = types.InputMediaPhoto(media=InputFile(f"materials/photos/{photo}.jpg"))
         
-        data = json.loads(db.get_storage(chat_id))
+        data = db.get_storage(chat_id)
         
-        data["photo"] = "dededede"
-        print(data)
+        data["photo"] = photo
         db.update_storage(chat_id, data)
         
         await bot.edit_message_media(
